c32846fb0dfe476597432d9de54efba38efa40aa
kjweb <kjt.web@gmail.com>
2021-11-18 10:47:45 +0900

    webp設定追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    formats: ['image/webp'],
     deviceSizes: [320, 420, 640, 750, 768, 828, 1024, 1080, 1200, 1920, 2048, 3840],
   },
 }
